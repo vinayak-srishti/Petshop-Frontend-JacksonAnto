18281202b50fc256f56bee1f437b946b1245f382
Vinith70 <158442521+Vinith70@users.noreply.github.com>
2025-02-03 15:02:57 +0530

    new files added

---
+++ src/Components/Petshop-folder/Addpets.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../Petshop-folder/Addpets.css"
+
+function Addpets() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Addpets
+
+ 
--- src/Components/Petshop-folder/Petshoplogin.js
+++ src/Components/Petshop-folder/Petshoplogin.js
@@ -1,21 +1,44 @@
 import React, { useState } from 'react'
 import "./Petshop.css"
 import "../../Assets/two.jpg"
-// import { FaLock } from "react-icons/fa";
-// import { FaUser, } from "react-icons/fa";
+import axios from 'axios'
 
 
 
 function Petshoplogin() {
-  const[login,setLogin]=useState({
-    username:"",
+  const[logins,setLogins]=useState({
+    email:"",
     password:""
   })
   const myfunction=((s)=>{
-    setLogin({
-        ...login,[s.target.name]:s.target.value
+    setLogins({
+        ...logins,[s.target.name]:s.target.value
     })
-    console.log(login)
+    console.log(logins)
+  })
+
+  const petlogin=((e)=>{
+    e.preventDefault()
+    if(logins.email,logins.password){
+      axios.post('http://localhost:3000/petshoplogin',logins)
+      .then((response)=>{
+        console.log(response);
+
+        if(response.data.status==200){
+          alert("login sucessfully")
+          
+        }
+        else if(response.data.status==404){
+          alert("please take the approval from the admin")
+        }
+        else if(response.data.status==500){
+          alert("Admin Reject Your Request")
+        }
+        else{
+          alert(response.data.msg)
+        }
+      })
+    }
   })
     return (
         <div className='total'>
@@ -23,20 +46,20 @@ function Petshoplogin() {
         <form action="">
           <h1>PETSHOP LOGIN</h1>
           <div class="input-box">
-            <input type="text" placeholder="Username" required/>
+            <input type="text"name='email'value={logins.email}onChange={myfunction} placeholder="E-Mail" required />
             {/* <FaUser className='icon' /> */}
           </div>
           <div class="input-box">
-            <input type="password" placeholder="Password" required/>
+            <input type="password" name="password"value={logins.password}onChange={myfunction} placeholder="Password" required/>
             {/* <FaLock className='icon' /> */}
           </div>
           <div class="remember-forgot">
             <label><input type="checkbox"/>Remember Me</label>
             <a href="#">Forgot Password</a>
           </div>
-          <button type="submit" class="btn">Login</button>
+          <button type="submit" class="btn"onClick={petlogin}>Login</button>
           <div class="register-link">
-            <p>Dont have an account? <a href="#">Register</a></p>
+            <p>Dont have an account? <a href="petregi">Register</a></p>
           </div>
         </form>
       </div>
