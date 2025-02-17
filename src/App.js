f0e698e9e44856b55527b80e789e40ce7e969451
Vinith70 <158442521+Vinith70@users.noreply.github.com>
2025-02-17 13:35:57 +0530

    new files

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ function App() {
     <BrowserRouter>
       <div>
         <Routes>
-          {/* user */}  
- <Route path="/" element={<Mainhomepage/>}/>
+          {/* user */}
+          <Route path="/" element={<Mainhomepage />} />
           <Route />
 
           <Route path="/*" element={<Nopage />} />
@@ -68,9 +68,9 @@ function App() {
           <Route path="/petshopreq" element={<Petshopreq />} />
           <Route />
           <Route path="profileview" element={<Profileview />} />
-          <Route path="navbar2" element={<Navbar2/>}/>
-                 <Route path="petshoplogin" element={<Petshoplogin />} />
-          <Route path="mainnavbar" element={<MainNavbar/>}/>
+          <Route path="navbar2" element={<Navbar2 />} />
+          <Route path="petshoplogin" element={<Petshoplogin />} />
+          <Route path="mainnavbar" element={<MainNavbar />} />
         </Routes>
       </div>
     </BrowserRouter>
