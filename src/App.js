87ae596b20f0d46eacfc2d7d490935e0e52e8833
j-akson <jackson003j@gmail.com>
2025-02-20 10:05:35 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
           <Route path="about" element={<About />}></Route>
           <Route path="addpet" element={<Addpets />}></Route>
           <Route path="/dogview/:id" element={<Dogview />}></Route>
-          <Route path="/addcart" element={<AddCart/>}></Route>
+          <Route path="/addcart" element={<AddCart />}></Route>
 
           {/* <Route path='/petss' element={<Viewpets/>}/> */}
           {/* <Route path='forgetpassword' element={<Forget/>}/>
